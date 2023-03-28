9c2b591256eca1a6d9b71f15147b49550bcb6f25
sixca <sixca0520@gmail.com>
2023-03-28 17:45:10 +0900

    0328 class commit

--- 모던스크립트/practice1.js
+++ 모던스크립트/practice1.js
@@ -1,1 +1,12 @@
 console.log("test");
+
+let person = function(){
+    name1 = "홍길동";
+    age = 20;
+    birth = [88,5];
+    country = undefined;
+    say = function(){
+        console.log("안녕하슈");
+    }
+}
+console.log(person);
