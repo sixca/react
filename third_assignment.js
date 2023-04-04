6cf7e3b90d84faee26a61ea76764e4dee3b955a8
sixca <124110460+sixca@users.noreply.github.com>
2023-04-04 18:25:56 +0900

    3rd assignment commit

--- third_assignment.js
+++ third_assignment.js
@@ -4,8 +4,6 @@ var ejs = require("ejs");
 
 var app = express();
 
-//app.use(express.urlencoded({extended:false}));
-
 app.get("/input", (request, response)=>{
     fs.readFile("./html/third_assignment.html", "utf-8", (err, data)=>{
         response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
@@ -36,13 +34,6 @@ app.get("/calc", (request, response)=>{
     response.send(`${name}님의 총점은 ${total}점이고 평균은 ${avg}점입니다`);
 });
 
-app.get("/clearCalc", (request, response)=>{
-    request.query.name = ""; 
-    request.query.ko = "";
-    request.query.eng = "";
-    request.query.mat = "";
-});
-
 app.use((request, response)=>{   
     response.writeHead(200,{'Content-Type':'text/html;charset=utf-8'});
     response.end("<H1>로얄 성적 계산기</H1>")      
@@ -53,5 +44,3 @@ app.listen(4000, ()=>{
 });  
 
 //http://127.0.0.1:4000/input
-//express 모듈의 메서드인 response.send();를 사용하면, response.writehead 구문은 생략해도 된다.
-//파일시스템 모듈을 사용하지 않는다면  let fs = require("fs"); 구문도 필요치 않다.
